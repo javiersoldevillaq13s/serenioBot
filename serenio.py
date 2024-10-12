7a31c219617a55fabfdca81119c33f71d136d000
Javier Soldevilla <145192861+javiersoldevillaq13s@users.noreply.github.com>
2024-10-12 13:03:33 -0500

    serenio.py

--- serenio.py
+++ serenio.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 # Configuración del modelo LLM
 llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0,
-                 api_key="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")
+                 api_key="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")
 
 # Título de la aplicación
 st.title("🤖 Serenio Bot")
